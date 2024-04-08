c9d918953dc9e5ecba6fb37da4bbfdcafc64a680
varun-4444 <umasammakka2011@gmail.com>
2024-04-08 19:10:42 +0530

    create a middle ware and a function to handle possible errors

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs';
-export const signup = async(req,res) =>
+export const signup = async(req,res,next) =>
 {
     const{username,email,password}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10);
@@ -11,6 +11,6 @@ export const signup = async(req,res) =>
     }
     catch(error)
     {
-        res.status(500).json(error.message);
+        next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -23,3 +23,17 @@ app.listen(3000,()=>
 app.use(express.json());
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+
+app.use((err,req,res,next) =>
+{
+    const statusCode=err.statusCode || 500;
+    const message = err.message || 'internal server error';
+    return res.status(statusCode).json(
+        {
+            success:false,
+            statusCode,             //from es6 if parameter and key has same name we can remove it
+            message,
+        }
+    );
+}
+);
