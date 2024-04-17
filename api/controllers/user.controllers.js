09a458a10fa5e84dc6200fba5608aec7cebe2432
varun-4444 <umasammakka2011@gmail.com>
2024-04-17 18:12:49 +0530

    create user Update Api

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -1,3 +1,8 @@
+import bcryptjs from 'bcryptjs'
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+
+
 export const test = (req,res)=>
 {
     res.json(
@@ -5,4 +10,29 @@ export const test = (req,res)=>
             message: 'An Api Route Is working',
         }
     );
+};
+export const updateUser= async(req,res,next)=>
+{
+    if(req.user.id !== req.params.id)
+    return next(errorHandler(401,"you Can only update your own account"))
+    try {
+        if(req.body.password)
+        {
+            req.body.password=bcryptjs.hashSync(req.body.password,10)        
+        }
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+            $set:
+            {   // set method because sometimes all form parameters are not filled for example i want update only user name and we shall not use rest operator because of admin operations
+                username:req.body.username,
+                email:req.body.email,
+                password:req.body.password,
+                avatar:req.body.avatar,
+            }
+        },{new:true}
+        )
+        const {password,...rest}=updatedUser._doc
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
+import cookieParser from 'cookie-parser'
 import cors from 'cors';
 
 dotenv.config();
@@ -25,6 +26,7 @@ mongoose.connect(process.env.MONGO).then(()=>
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 
